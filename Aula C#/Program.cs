9f902f9a5597cc605b39363a2020155293deb33b
DROGA EH O VINNI <vgvaconcelos6@gmail.com>
2026-02-22 19:53:55 -0300

    Adicionar arquivos de projeto.

---
+++ Aula C#/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Qual o seu nome?");
+string nome = Console.ReadLine();
+Console.WriteLine("Qual a sua idade?");
+int idade = int.Parse(Console.ReadLine());
+int anoN = DateTime.Now.Year - idade;
+Console.WriteLine($"Olá, {nome}, sua idade é {idade} então você provavelmente nasceu em {anoN}.");
