70beb4c94304144d5670e06e76369c87eaaeb438
DROGA EH O VINNI <vgvaconcelos6@gmail.com>
2026-03-07 15:28:54 -0300

    Atividade 03 Avaliador de Empréstimos

---
+++ Aula 01/Atividade 03/Atividade 03/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("---CUBANK---");
+Console.WriteLine("Por favor, informe o ano do seu nascimento:");
+int anoN = int.Parse(Console.ReadLine());
+int idade = DateTime.Now.Year - anoN;
+Console.WriteLine("Agora informe sua renda mensal:");
+double renda = double.Parse(Console.ReadLine());
+Console.WriteLine("Possui alguma divida ou pendencia em seu nome?");
+Console.WriteLine("Digite '1' para 'sim' e '2' para 'não'");
+int serasa = int.Parse(Console.ReadLine());
+
+if (idade < 18)
+{
+    Console.Write("Infelizmente você não possui idade para aprovarmos o emprestimo!");
+}
+else if (renda >= 2500.00 && serasa == 2)
+{
+    Console.Write("---EMPRESTIMO APROVADO!---");
+}
+else
+{
+    Console.Write("---EMPRESTIMO NEGADO!!!---");
+}
